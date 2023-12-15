5ddabe186f699eeb0904427d13a95f94c192cf92
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2023-12-15 11:38:16 -0300

    add tema repository, controller e CRUD completo

---
+++ src/main/java/br/net/impacthub/repository/TemaRepository.java
@@ -0,0 +1,15 @@
+package br.net.impacthub.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import br.net.impacthub.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	
+	public List<Tema> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+	public List<Tema> findAllByUrgenciaContainingIgnoreCase(@Param("nivelUrgencia") String nivelUrgencia);
+
+}
